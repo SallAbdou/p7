e985168dcec57f13512850d2bfe28c1562c3d81f
Abdou <sall.abdou34@gmail.com>
2024-03-29 13:59:05 +0100

    test tailwind/config à refaire eslint et sass

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'anton': ['Anton', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
